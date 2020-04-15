a6f0c4fcdd931f9fad52e8102c13818313510fb7
Fredrik Loch <mail@fredrikloch.me>
2020-04-15 07:47:02 +0200

    Added styling

--- src/components/bingo.js
+++ src/components/bingo.js
@@ -1,4 +1,5 @@
 import React from "react"
+import containerStyles from "./container.module.css"
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -11,7 +12,8 @@ function shuffleArray(array) {
 }
 
 export default class Bingo extends React.Component{
-    letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z','å','ä','ö']
+    letters = ['B','C','D','F','G','H','K','P','Q','T','U','W','X','Y','Z','Å','Ä','Ö','b','c','d','f','g','h','k','p','q','t','u','w','x','y','z','å','ä','ö']
+    played = ""
     constructor(props){
       super(props)
       this.letters = shuffleArray(this.letters)
@@ -21,17 +23,26 @@ export default class Bingo extends React.Component{
     }
 
     handleClick = () => {
-      let buttonText = this.letters.length > 0 ? this.letters.pop() : "Alla bokstäver har dragits"
-      this.setState({buttonText: buttonText})
+        let buttonText = this.letters.length > 0 ? this.letters.pop() : "Alla bokstäver har dragits"
+        if(buttonText != "Alla bokstäver har dragits"){
+          this.played += " " + buttonText
+        }
+        this.setState({buttonText: buttonText})
     }
 
     render(){
       return (
         <div>
             <p>Tryck på knappen för att ta fram en ny bokstav</p>
-            <button onClick={this.handleClick}>Snurra</button>
-            <div>
-                {this.state.buttonText}
+            <button className={containerStyles.button} onClick={this.handleClick}>Snurra</button>
+            <br></br>
+            <div className={containerStyles.card}>
+                <p>
+                    {this.state.buttonText}
+                </p>
+            </div>
+            <br></br>
+            <div><p>Spelade bokstäver:</p> {this.played}
             </div>
         </div>
       )
